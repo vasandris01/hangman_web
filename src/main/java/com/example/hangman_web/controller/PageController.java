63f2ee60786bfb1c58a4ab489137bfb732ae4aed
vasandris01 <undris01@gmail.com>
2023-09-18 19:35:14 +0200

    game end logic

--- src/main/java/com/example/hangman_web/controller/PageController.java
+++ src/main/java/com/example/hangman_web/controller/PageController.java
@@ -11,16 +11,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class PageController {
     Game game = new Game();
-    @GetMapping({"","/","/home"})
-    public String getHome(Model model){
+
+    @GetMapping({"", "/", "/home"})
+    public String getHome(Model model) {
         model.addAttribute("input", new UserInput());
         model.addAttribute("game", game);
         return "home";
     }
 
     @PostMapping("/play")
-    public String choose(@ModelAttribute("input") UserInput input){
+    public String choose(@ModelAttribute("input") UserInput input) {
         game.useChoice(input.getChoice());
+        if (game.isEnd()) {
+            return "redirect:/game_end";
+        }
         return "redirect:/home";
+    }
+
+    @GetMapping("/game_end")
+    public String gameEnd(Model model) {
+        model.addAttribute("message", game.getLife() == 0 ? "noob" : "gg");
+        return "game_end";
     }
 }
--- src/main/java/com/example/hangman_web/model/Game.java
+++ src/main/java/com/example/hangman_web/model/Game.java
@@ -1,16 +1,30 @@
 package com.example.hangman_web.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Game {
     private String word;
     private String wordAtLine;
-
+    private int life = 10;
+    private Set<Character> inputs = new HashSet<>();
+    private boolean isEnd = false;
     public Game(){
         word = "macska";
         wordAtLine = "_".repeat(word.length());
     }
 
     public void useChoice(String choice){
-
+        if(inputs.add(choice.charAt(0))){
+            if(isInTheWord(choice)){
+                overRideLine(choice);
+            } else {
+                --life;
+            }
+        }
+        if(life == 0 || word.equals(wordAtLine)){
+            isEnd = true;
+        }
     }
 
     private void overRideLine(String input){
@@ -42,5 +56,29 @@ public class Game {
 
     public void setWordAtLine(String wordAtLine) {
         this.wordAtLine = wordAtLine;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
+    }
+
+    public Set<Character> getInputs() {
+        return inputs;
+    }
+
+    public void setInputs(Set<Character> inputs) {
+        this.inputs = inputs;
+    }
+
+    public boolean isEnd() {
+        return isEnd;
+    }
+
+    public void setEnd(boolean end) {
+        isEnd = end;
     }
 }

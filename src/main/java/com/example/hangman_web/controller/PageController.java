7741640f379b5892f5773ee53e341cd46a37a1aa
vasandris01 <undris01@gmail.com>
2023-09-18 18:13:30 +0200

    homePage v1

--- src/main/java/com/example/hangman_web/controller/PageController.java
+++ src/main/java/com/example/hangman_web/controller/PageController.java
@@ -1,2 +1,22 @@
-package com.example.hangman_web.controller;public class PageController {
+package com.example.hangman_web.controller;
+
+import com.example.hangman_web.model.UserInput;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class PageController {
+    @GetMapping({"","/","/home"})
+    public String getHome(Model model){
+        model.addAttribute("input", new UserInput());
+        return "home";
+    }
+
+    @PostMapping("/play")
+    public String choose(@ModelAttribute("input") UserInput input){
+        return "redirect:/home";
+    }
 }
--- src/main/java/com/example/hangman_web/model/UserInput.java
+++ src/main/java/com/example/hangman_web/model/UserInput.java
@@ -1,2 +1,14 @@
-package com.example.hangman_web.model;public class UserInput {
+package com.example.hangman_web.model;
+
+public class UserInput {
+    private String choice;
+
+    public String getChoice() {
+        return choice;
+    }
+
+    public void setChoice(String choice) {
+        this.choice = choice;
+    }
 }
+
